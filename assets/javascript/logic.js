53073dfe4236798b6d07bc574ceb789abc0d554f
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2018-11-14 14:31:52 -0700

    added javascript to append information to temporary index page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+
+    function appendMovie(response) {
+        $(".display").empty();
+        var movieTitle = $("<p>").text(response.Title);
+        var movieRated = $("<p>").text(response.Rated);
+        var movieActors = $("<p>").text(response.Actors);
+        var movieGenre = $("<p>").text(response.Genre);
+        var moviePlot = $("<p>").text(response.Plot);
+        var movieCritic = $("<p>").text(response.Ratings[2].Source);
+        var movieCriticRating = $("<p>").text(response.Ratings[2].Value);
+        var moviePosterUrl = response.Poster;
+        // console.log(movieTitle, movieRated, movieActors, movieCritic, movieCriticRating, moviePlot, moviePosterUrl, movieGenre);
+        $("#movie-title").append(movieTitle);
+        $("#movie-rated").append(movieRated);
+        $("#movie-actors").append(movieActors);
+        $("#movie-genre").append(movieGenre);
+        $("#movie-plot").append(moviePlot);
+        $("#movie-critic").append(movieCritic);
+        $("#movie-rating").append(movieCriticRating);
+        $("#movie-poster").attr("src", moviePosterUrl);
+    };
+
     $("#add-movie").on("click", function (event) {
 
         event.preventDefault();
@@ -20,6 +42,8 @@ $(document).ready(function () {
             method: 'GET',
         }).done(function (response) {
             console.log(response);
+            appendMovie(response);
+
         }).fail(function (err) {
             throw err;
         });
