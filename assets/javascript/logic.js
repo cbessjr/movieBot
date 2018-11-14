05a911f2287705cb206ad609207e1ef6c49d4335
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2018-11-14 15:50:59 -0700

    jquery adds buttons, added firebase-stores movie titles to generate buttons

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC2lAryNx_K6KKAzVKVihZ5JEie983QHt0",
+        authDomain: "movie-search-b282f.firebaseapp.com",
+        databaseURL: "https://movie-search-b282f.firebaseio.com",
+        projectId: "movie-search-b282f",
+        storageBucket: "movie-search-b282f.appspot.com",
+        messagingSenderId: "248808525127"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
 
+    //function to append movies to page
     function appendMovie(response) {
         $(".display").empty();
         var movieTitle = $("<p>").text(response.Title);
@@ -19,6 +31,19 @@ $(document).ready(function () {
         $("#movie-critic").append(movieCritic);
         $("#movie-rating").append(movieCriticRating);
         $("#movie-poster").attr("src", moviePosterUrl);
+
+
+    };
+
+    //function to add buttons to page
+    function addButton(childSnapshot) {
+        var databaseTitle = childSnapshot.val().movieTitle;
+        var buttonContainer = $("#button-container")
+        var newButton = $("<button/>").text(databaseTitle)
+            .attr("data-value", databaseTitle)
+            .attr("class", "movie-button");
+
+        buttonContainer.append(newButton)
     };
 
     $("#add-movie").on("click", function (event) {
@@ -26,6 +51,36 @@ $(document).ready(function () {
         event.preventDefault();
 
         var searchInput = $("#movie-input").val().trim();
+
+        var url = "http://www.omdbapi.com/";
+        url += '?' + $.param({
+            //"i": "tt3896198",
+            "t": searchInput, //user input search by movie name
+            "plot": "short", //returns shortened plot
+            "apikey": "adaf8b76" //api-key
+        });
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (response) {
+            console.log(response);
+            appendMovie(response);
+            //addButton(response);
+            var firebaseButton = {
+                movieTitle: response.Title
+            };
+            database.ref().push(firebaseButton);
+
+        }).fail(function (err) {
+            throw err;
+        });
+
+    });//end first on click
+
+    $(document).on("click", ".movie-button", function (event) {
+
+        var searchInput = $(this).attr("data-value");
         //var typeInput = $("#example-input-field").val().trim();
 
         var url = "http://www.omdbapi.com/";
@@ -43,10 +98,15 @@ $(document).ready(function () {
         }).done(function (response) {
             console.log(response);
             appendMovie(response);
+            // addButton(response);
 
         }).fail(function (err) {
             throw err;
         });
+    });//end second on click
 
-    });
-});
+    database.ref().on("child_added", function (childSnapshot) {
+        console.log(childSnapshot);
+        addButton(childSnapshot);
+    });//end first on child_added
+});//end code
